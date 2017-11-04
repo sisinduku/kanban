fe3e77ade506d0365135af9d7ec9912a2447864d
sisinduku <saptanto.sindu@gmail.com>
2017-11-04 22:39:29 +0700

    Add Task Component

---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="TaskComponent">
+    <b-card style="max-width: 20rem;"
+            class="mb-4"
+            :header="task.title">
+      <p class="card-text text-left">
+        Point: {{task.point}}
+      </p>
+      <p class="card-text text-left">
+        Assigned to: {{task.user}}
+      </p>
+      <b-button variant="primary" @click="showDetail">Show Detail</b-button>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TaskComponent',
+
+  props: ['task'],
+
+  methods: {
+    showDetail () {
+      this.$store.commit('toggleDetail', this.task)
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
